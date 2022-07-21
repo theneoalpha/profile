d251c246a6c7c5c00ef6d3f6326294f014116647
theneoalpha <103592069+theneoalpha@users.noreply.github.com>
2022-07-21 21:52:30 +0530

    make it better

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ const PORT = process.env.PORT || 8000;
 
 
 //Lets try it
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("./client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {`enter code here`
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-}
+if(process.env.NODE.ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+    
+  }
 
 app.listen(PORT,()=>{
 console.log("Listening at port 5000");
